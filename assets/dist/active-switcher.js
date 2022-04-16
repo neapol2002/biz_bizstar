92dd858015b3096ccdc6cff9063a5f4fed8cb3ba
neapol2002 <neapol2002@mail.ru>
2022-04-16 16:27:53 +0400

    Bizines tmpl init

---
+++ assets/dist/active-switcher.js
@@ -0,0 +1,50 @@
+
+var colorSheets = [
+    {
+        color: "#ffac00",
+        title: "Switch to Default",
+        href: "./assets/css/color/color-default.css"
+    },
+    {
+        color: "#ed1c24",
+        title: "Switch to Red",
+        href: "./assets/css/color/color-red.css"
+    },
+    {
+        color: "#94C523",
+        title: "Switch to Green",
+        href: "./assets/css/color/color-green.css"
+    },
+    {
+        color: "#1AB7D8",
+        title: "Switch to Blue",
+        href: "./assets/css/color/color-blue.css"
+    },
+    {
+        color: "#F57D41",
+        title: "Switch to Orange",
+        href: "./assets/css/color/color-orange.css"
+    },
+    {
+        color: "#4676fa",
+        title: "Switch to Sky",
+        href: "./assets/css/color/color-sky.css"
+    },
+    {
+        color: "#f92c8b",
+        title: "Switch to Pink",
+        href: "./assets/css/color/color-pink.css"
+    },
+    {
+        color: "#AA00FF",
+        title: "Switch to Purple",
+        href: "./assets/css/color/color-purple.css"
+    },
+    {
+        color: "#CDDC39",
+        title: "Switch to Lime",
+        href: "./assets/css/color/color-lime.css"
+    }
+];
+
+ColorSwitcher.init(colorSheets);
